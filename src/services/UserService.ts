86d2decfa768d4a69b6e81d80d2093c4b023c6d0
taaha47 <taaha.ham@gmail.com>
2019-05-19 03:24:35 +0100

    fix temporary auth method.

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -13,8 +13,8 @@ export const UserServiceCb: any = async (dbConnection: any, element: ipcPayload,
       const {password} = payload;
       try {
         const user = await userRepo.createQueryBuilder("user").where("user.username = :username", {username: "admin"}).getOne();
-        const auth = await bcrypt.compare(user.passwordHash, password);
-        event.returnValue = "success";
+        //const auth = await bcrypt.compare(user.passwordHash, password);
+        event.returnValue = user.passwordHash === password ? "success" : "error";
       } catch(e) {
         event.returnValue = "error";
       }
