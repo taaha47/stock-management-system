03d91cff5657768573eb40bce7cf8f867a090bc3
taaha47 <taaha.ham@gmail.com>
2019-07-08 00:01:43 +0100

    add form, submit form to EditCategoryFormPopup component.

--- src/components/CategoriesListContainer/EditCategoryFormPopup.vue
+++ src/components/CategoriesListContainer/EditCategoryFormPopup.vue
@@ -12,55 +12,67 @@
       <v-card-title>
         <span class="headline">Modifier la categorie <span class="green--text"> {{element.category_code}} | {{element.category_name}} </span></span>
       </v-card-title>
-      <v-card-text>
-        <v-container grid-list-md>
-          <v-flex xs12 sm6 md6>
-            <v-text-field
-              label="Code de la catégorie"
-              hint="entrer un code unique pour identifier la catégorie"
-              required
-              :value="element.category_code"
-            ></v-text-field>
-            <v-spacer></v-spacer>
-          </v-flex>
-          <v-flex xs12 sm6 md6>
-            <v-text-field
-              label="Nom de la catégorie"
-              hint="Choisir un nom pour la catégorie"
-              required
-              :value="element.category_name"
-            ></v-text-field>
-            <v-spacer></v-spacer>
-          </v-flex>
-          <v-flex xs12 sm12 md12>
-            <v-textarea
-              label="description de la catégorie"
-              hint="Donner une description pour cette catégorie"
-              required
-              auto-grow
-              rows="1"
-              :value="element.category_description"
-            ></v-textarea>
-          </v-flex>
-        </v-container>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          color="green darken-1"
-          flat
-          @click="() => {editElement(element); dialog=false}"
-        >
-          Confirmer
-        </v-btn>
-        <v-btn
-          color="red darken-1"
-          flat
-          @click="dialog=false"
-        >
-          Annuler
-        </v-btn>
-      </v-card-actions>
+      <v-form
+        v-model="valid"
+        @submit.prevent="handleSubmit"
+      >
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-flex xs12 sm6 md6>
+              <v-text-field
+                label="Code de la catégorie"
+                hint="entrer un code unique pour identifier la catégorie"
+                required
+                v-model="categoryCode"
+                id="categoryCode"
+                name="categoryCode"
+                :rules="rules"
+              ></v-text-field>
+              <v-spacer></v-spacer>
+            </v-flex>
+            <v-flex xs12 sm6 md6>
+              <v-text-field
+                label="Nom de la catégorie"
+                hint="Choisir un nom pour la catégorie"
+                required
+                v-model="categoryName"
+                id="categoryName"
+                name="categoryName"
+                :rules="rules"
+              ></v-text-field>
+              <v-spacer></v-spacer>
+            </v-flex>
+            <v-flex xs12 sm12 md12>
+              <v-textarea
+                label="description de la catégorie"
+                hint="Donner une description pour cette catégorie"
+                auto-grow
+                rows="1"
+                v-model="categoryDescription"
+                id="categoryDescription"
+                name="categoryDescription"
+              ></v-textarea>
+            </v-flex>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            flat
+            type="submit"
+          >
+            Confirmer
+          </v-btn>
+          <v-btn
+            color="red darken-1"
+            flat
+            @click="dialog=false"
+          >
+            Annuler
+          </v-btn>
+        </v-card-actions>
+      </v-form>
     </v-card>
   </v-dialog>
 </template>
@@ -72,6 +84,18 @@
     @Prop() readonly editElement: Function;
     @Prop() readonly element: any;
 
+    categoryCode: string = this.element.category_code;
+    categoryName: string = this.element.category_name;
+    categoryDescription: string = this.element.category_description;
     dialog: boolean = false;
+    rules: any[] = [(v: boolean) => !!v || "Ce champ est obligatoire"];
+
+    handleSubmit() {
+      this.editElement({
+        "category_code": this.categoryCode,
+        "category_name": this.categoryName,
+        "category_description": this.categoryDescription
+      });
+    }
   }
 </script>
